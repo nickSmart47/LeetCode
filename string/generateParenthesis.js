e6d4996bf86059a67401f3961042d9d2355d4787
Nick Smart <nsmart147@gmail.com>
2022-03-18 21:58:10 -0400

    started generate parentheses

---
+++ string/generateParenthesis.js
@@ -0,0 +1,28 @@
+/**
+ *  {Given n pairs of parentheses, write a function to generate all combinations of well-formed parentheses.
+
+Example 1:
+
+Input: n = 3
+Output: ["((()))","(()())","(())()","()(())","()()()"]
+Example 2:
+
+Input: n = 1
+Output: ["()"]} n 
+ */
+
+
+/**
+ * @param {number} n
+ * @return {string[]}
+ */
+var generateParenthesis = function (n) {
+    let result = [];
+
+    for (let i = 0; i < n; i++){
+        let combination = "";
+        
+    }
+};
+
+console.log(generateParenthesis(n=3)) // ["((()))","(()())","(())()","()(())","()()()"]
