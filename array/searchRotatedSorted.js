ae3179b261d1b0d8f59fde13b8c43e0c6bd421c5
Nick Smart <nsmart147@gmail.com>
2022-04-04 11:02:46 -0400

    binary search solution added, faster than 88%

--- array/searchRotatedSorted.js
+++ array/searchRotatedSorted.js
@@ -10,8 +10,28 @@ You must write an algorithm with O(log n) runtime complexity.
  * @param {number} target
  * @return {number}
  */
-var search = function (nums, target) {
-    return nums.indexOf(target);
-};
+// var search = function (nums, target) {
+//     return nums.indexOf(target);
+// };
 
-console.log(search(nums = [4,5,6,7,0,1,2], target = 0))
+var search = (nums, target) => {
+    let start = 0, end = nums.length - 1;
+    while (start <= end) {
+        let mid = Math.floor((start + end) / 2);
+
+        if (nums[mid] == target) return mid;
+
+        if (nums[mid] >= nums[start]) {
+            if (nums[mid] > target && target >= nums[start]) {
+                end = mid - 1;
+            } else start = mid + 1;
+        }
+        else {
+            if (nums[mid] <= target && target <= nums[end]) {
+                start = mid + 1;
+            } else end = mid - 1;
+        }
+    }
+    return -1;
+}
+console.log(search(nums = [4, 5, 6, 7, 0, 1, 2], target = 0))
