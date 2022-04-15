1f04eca3b5714e95920cac8cab48b3ca00392c81
Nick Smart <nsmart147@gmail.com>
2022-04-15 12:36:57 -0400

    working solution

--- dynamic_programming/generateParenthesis.js
+++ dynamic_programming/generateParenthesis.js
@@ -1,16 +1,35 @@
 /**
  * Given n pairs of parentheses, write a function to generate all combinations of well-formed parentheses.
+ * 
  * Example 1:
-
 Input: n = 3
 Output: ["((()))","(()())","(())()","()(())","()()()"]
-Example 2:
 
+decision tree?
+so one possibility is to put n opening parens first...
+then can do n-1 opening parens first... 
+and so on...
+
+Example 2:
 Input: n = 1
 Output: ["()"]
+
  * @param {number} n
  * @return {string[]}
  */
  var generateParenthesis = function(n) {
-    
+    const results = [];
+
+    const generateCombinations = (open = 0, close = 0, parenString = '') => {
+        if (close === n){
+            results.push(parenString);
+            return;
+        }
+        if (open < n) generateCombinations(open+1, close, parenString + '(');
+        if (open > close) generateCombinations(open, close + 1, parenString + ')');
+    }
+    generateCombinations();
+    return results;
 };
+
+console.log(generateParenthesis(3))
