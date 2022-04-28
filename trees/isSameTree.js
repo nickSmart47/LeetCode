a9ae9a3ebb7fe6731a7050e69a149a4765be2f62
Nick Smart <nsmart147@gmail.com>
2022-04-27 23:01:52 -0400

    working recursive solution for isSameTree

--- trees/isSameTree.js
+++ trees/isSameTree.js
@@ -14,22 +14,26 @@ Two binary trees are considered the same if they are structurally identical, and
  * @param {TreeNode} q
  * @return {boolean}
  */
-function TreeNode(val, left, right){
-    this.val = (val===undefined ? 0 : val)
-    this.left = (left===undefined ? null : left)
-    this.right = (right===undefined ? null : right)
+function TreeNode(val, left, right) {
+  this.val = val === undefined ? 0 : val;
+  this.left = left === undefined ? null : left;
+  this.right = right === undefined ? null : right;
 }
 
- var isSameTree = function(p, q) {
-    if (p.val == q.val){
-        if (p.left == q.left){
-            if (p.right == q.right){
-                return true;
-            }
-        }
-    }
-    return false;
+var isSameTree = function (p, q) {
+  if(!p && !q) return true;
+
+  if (!p || !q) return false;
+
+  if (p.val !== q.val) return false;
+
+  if (!isSameTree(p.left, q.left)) return false;
+  if (!isSameTree(p.right, q.right)) return false;
+
+  return true;
 };
 
-let p = TreeNode(1,2,3);
-let q = TreeNode(1,2,3);
+let p = new TreeNode(1, 2, 3);
+let q = new TreeNode(1, 2, 3);
+
+console.log(isSameTree(p, q));
